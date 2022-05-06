1f05395c355b7e473655fa9b9ec4eb283ffca06b
tranbuutai <tranbuutai80@gmail.com>
2022-05-06 10:53:40 +0700

    add commit

--- src/components/Common/Home.jsx
+++ src/components/Common/Home.jsx
@@ -24,7 +24,7 @@ export function Home() {
             size="lg"
             onClick={() => handleButtonClick()}
           >
-            ........ here
+            ....... here
           </Button>
         </Link>
       </div>
