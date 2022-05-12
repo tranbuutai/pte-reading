3ff17c8cb66770874295741b9ab2fc62cb491900
tranbuutai <tranbuutai80@gmail.com>
2022-05-12 15:05:19 +0700

    add 2nd env

--- src/components/Common/Home.jsx
+++ src/components/Common/Home.jsx
@@ -24,7 +24,7 @@ export function Home() {
             size="lg"
             onClick={() => handleButtonClick()}
           >
-            ....... here
+            ....... here {process.end.REACT_APP_ASD}
           </Button>
         </Link>
       </div>
