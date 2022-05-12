3ba7b4edafb85bf98ba17cbffd4231099029559f
tranbuutai <tranbuutai80@gmail.com>
2022-05-12 15:13:24 +0700

    fix

--- src/components/Common/Home.jsx
+++ src/components/Common/Home.jsx
@@ -24,7 +24,7 @@ export function Home() {
             size="lg"
             onClick={() => handleButtonClick()}
           >
-            ....... here {process.end.REACT_APP_ASD}
+            ....... here {process.env.REACT_APP_ASD}
           </Button>
         </Link>
       </div>
